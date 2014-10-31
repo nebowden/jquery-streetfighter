0c9809c71baca531b0d41d79d51f13424383748f
nebowden <nathanebowden@gmail.com>
2014-10-31 10:57:19 -0500

    added hadouken throwing animation and sound

--- js/app.js
+++ js/app.js
@@ -6,5 +6,33 @@ $(document).ready(function(){
 	.mouseleave(function() {
 		$(".ryu-still").show();
 		$(".ryu-ready").hide();
+	})
+	.mousedown(function() {
+		playHadouken();
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'}, 
+			500, 
+			function() {
+				$(this).hide();
+				$(this).css('left', '-212px');
+			}
+		);
+	})
+		// console.log('mousedown');
+		// play hadouken sound
+	.mouseup(function() {
+		$(".ryu-ready").show();
+		$(".ryu-throwing").hide();
+		// console.log('mouseup');
+		//ryu goes back to his ready position
 	});
 })
+
+function playHadouken () {
+		$('#hadouken-sound')[0].volume=0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
